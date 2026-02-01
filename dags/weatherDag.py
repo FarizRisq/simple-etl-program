5d4b357b4723d19c073a2ff44ffd0d763e477009
FarizRisq <125780487+FarizRisq@users.noreply.github.com>
2026-02-01 13:40:12 +0700

    Refactor weather DAG to use new PythonOperator imports

--- dags/weatherDag.py
+++ dags/weatherDag.py
@@ -1,58 +1,52 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
 
 from src.get_weather import get_weather
-from src.transform_data import transform_data
+from src.transform_data import transform_weather
 from src.load_table import load_table
-import requests
-import json
-import os
 
-
-# Define the default dag arguments.
+# ===============================
+# DEFAULT ARGS
+# ===============================
 default_args = {
-    'owner': 'Fariz',
-    'depends_on_past': False,
-    'email': ['farizr.official@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 5,
-    'retry_delay': timedelta(minutes=1)
+    "owner": "Fariz",
+    "depends_on_past": False,
+    "email": ["farizr.official@gmail.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=1),
 }
 
-
-# Define the dag, the start date and how frequently it runs.
-# I chose the dag to run everday by using 1440 minutes.
-dag = DAG(
-    dag_id='weatherDag',
+# ===============================
+# DAG DEFINITION
+# ===============================
+with DAG(
+    dag_id="weather_dag",
     default_args=default_args,
-    start_date=datetime(2025, 8, 18),
-    schedule_interval=timedelta(minutes=1440))
+    start_date=days_ago(1),
+    schedule_interval="@daily",
+    catchup=False,
+    tags=["weather", "etl"],
+) as dag:
 
+    extract = PythonOperator(
+        task_id="extract_weather",
+        python_callable=get_weather,
+    )
 
-# First task is to query get the weather from openweathermap.org.
-task1 = PythonOperator(
-    task_id='get_weather',
-    provide_context=True,
-    python_callable=get_weather,
-    dag=dag)
+    transform = PythonOperator(
+        task_id="transform_weather",
+        python_callable=transform_weather,
+        op_args=["{{ ti.xcom_pull(task_ids='extract_weather') }}"],
+    )
 
+    load = PythonOperator(
+        task_id="load_table",
+        python_callable=load_table,
+        op_args=["{{ ti.xcom_pull(task_ids='transform_weather') }}"],
+    )
 
-# Second task is to transform the data
-task2 = PythonOperator(
-    task_id='transform_data',
-    provide_context=True,
-    python_callable=transform_data,
-    dag=dag)
-
-# Third task is to load data into the database.
-task3 = PythonOperator(
-    task_id='load_table',
-    provide_context=True,
-    python_callable=load_table,
-    dag=dag)
-
-# Set task1 "upstream" of task2
-# task1 must be completed before task2 can be started
-task1 >> task2 >> task3
+    extract >> transform >> load
